16d7e738164df0bedbdaf8730da75a2337b39fc9
shanshel6 <shanshel6@users.noreply.github.com>
2026-02-26 22:46:20 +0800

    Cleanup_Prisma_Client_and_re-push

--- server/prismaClient.js
+++ server/prismaClient.js
@@ -10,26 +10,7 @@ const envPath = path.join(__dirname, '.env');
 dotenv.config({ path: envPath });
 
 const prisma = new PrismaClient({
-    log: ['query', 'error', 'warn'],
+    log: ['error'],
 });
-
-async function testConnection() {
-    try {
-        await prisma.$connect();
-        console.log('--- DATABASE CONNECTED SUCCESSFULLY ---');
-    } catch (error) {
-        console.error('--- DATABASE CONNECTION ERROR DETAILS ---');
-        console.error('Message:', error.message);
-        console.error('Code:', error.code);
-        console.error('Environment DATABASE_URL:', process.env.DATABASE_URL ? 'DEFINED' : 'UNDEFINED');
-        if (process.env.DATABASE_URL) {
-            const url = new URL(process.env.DATABASE_URL);
-            console.log('DB Host:', url.hostname);
-            console.log('DB Port:', url.port);
-        }
-    }
-}
-
-testConnection();
 
 export default prisma;
